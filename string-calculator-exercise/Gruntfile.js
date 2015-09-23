b733100d22c4b141eec57dd878b5e6e90bbe113a
tiennguyen2501 <ntttien251@gmail.com>
2015-09-23 14:50:46 +0700

    Creating a project to do String Calculator exercise

---
+++ string-calculator-exercise/Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		  log: {
+		    foo: [1, 2, 3],
+		    bar: 'hello world',
+		    baz: false
+		  }
+		});
+
+		grunt.registerMultiTask('log', 'Log stuff.', function() {
+		  grunt.log.writeln(this.target + ': ' + this.data);
+		});
+};
